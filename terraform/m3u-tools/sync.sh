df87f96abb442cc03b1b169fba41899c7f5821c4
Matt Elliott <mattadamelliott@gmail.com>
2024-05-15 15:28:12 -0400

    updated sync script for m3u tools

--- terraform/m3u-tools/sync.sh
+++ terraform/m3u-tools/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avz --progress --include '*.py' --exclude '*' --rsync-path="sudo -u hashi rsync" . matt@jerry.shamsway.net:/mnt/services/iptvtools/
+rsync -avz --progress --include '*.py' --include '*.ini' --exclude '*' --rsync-path="sudo -u hashi rsync" . matt@jerry.shamsway.net:/mnt/services/iptvtools/
